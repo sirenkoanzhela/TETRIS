5b33f465c94f821cf39b72434a6edddd729e6afd
Anzhela Sirenko <MalfoyD@i.ua>
2021-06-03 13:44:04 +0300

    add member in List initialization

--- VS_FILES/ConsolEngine/Field.cpp
+++ VS_FILES/ConsolEngine/Field.cpp
@@ -2,9 +2,9 @@
 
 Field::Field(BaseApp* _game)
 {
-	for (int x = 0; x < _game->X_SIZE + 1; x++)//
+	for (int x = 0; x < _game->X_SIZE + 1; x++)
 	{
-		for (int y = 0; y < _game->Y_SIZE + 1; y++)//
+		for (int y = 0; y < _game->Y_SIZE + 1; y++)
 		{
 			if ((x == 0) || (x == _game->X_SIZE) || (x == _game->X_SIZE - 9) || (y == 0) || (y == _game->Y_SIZE - 5) || (y == _game->Y_SIZE))
 			{
